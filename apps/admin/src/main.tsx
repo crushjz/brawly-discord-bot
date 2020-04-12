0d297881356e02700fd6204f02a18c6c20e06fcc
Cesare Puliatti <crushjz@gmail.com>
2020-04-12 15:03:25 +0200

    Add rebassjs UI library

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,13 +1,17 @@
+import theme from '@rebass/preset'
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import { BrowserRouter } from 'react-router-dom'
-
+import { ThemeProvider } from 'styled-components'
 import App from './app/app'
+
+/* eslint-disable functional/no-expression-statement */
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById('root')
 )
