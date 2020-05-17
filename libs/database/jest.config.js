0be39b5f5392302c16dc5e408d9344ad76274620
Cesare Puliatti <crushjz@gmail.com>
2020-05-17 16:15:28 +0200

    Feature: setup database with prisma (#2)
    Setup database with prisma

---
+++ libs/database/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'database',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/libs/database',
+}
---
+++ libs/database/src/schema/new-db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "public"."User" (
+	"id" SERIAL PRIMARY KEY,
+	"email" VARCHAR(255) NOT NULL,
+	"discordToken" VARCHAR(255) UNIQUE NOT NULL
+);
